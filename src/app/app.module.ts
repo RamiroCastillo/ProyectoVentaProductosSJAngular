5fa1581bf0d67500007b60f8d7ad241b8ddbe3f2
Ramiro Castillo <castilloramiro313@gmail.com>
2019-03-03 16:42:46 -0400

    Creacion de componente Home y Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { OfertasComponent } from './Componentes/ofertas/ofertas.component';
 import { PuntosVentaComponent } from './Componentes/puntos-venta/puntos-venta.component';
 import { EventosComponent } from './Componentes/eventos/eventos.component';
 import { ContactosComponent } from './Componentes/contactos/contactos.component';
+import { ContenedorComponent } from './Componentes/contenedor/contenedor.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { HeaderComponent } from './Componentes/header/header.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ContactosComponent } from './Componentes/contactos/contactos.component'
     OfertasComponent,
     PuntosVentaComponent,
     EventosComponent,
-    ContactosComponent
+    ContactosComponent,
+    ContenedorComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
