4c892749f029b6fcc24c71f0117df61a5ca292a9
Ramiro Castillo <castilloramiro313@gmail.com>
2019-03-02 21:02:25 -0400

    Creacion de Componentes barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarLateralComponent } from './Componentes/nav-bar-lateral/nav-bar-lateral.component';
 import { NavbarSuperiorComponent } from './Componentes/nav-bar-lateral/navbar-superior/navbar-superior.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
+import { RecetasComponent } from './Componentes/recetas/recetas.component';
+import { OfertasComponent } from './Componentes/ofertas/ofertas.component';
+import { PuntosVentaComponent } from './Componentes/puntos-venta/puntos-venta.component';
+import { EventosComponent } from './Componentes/eventos/eventos.component';
+import { ContactosComponent } from './Componentes/contactos/contactos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarLateralComponent,
-    NavbarSuperiorComponent
+    NavbarSuperiorComponent,
+    ProductoComponent,
+    RecetasComponent,
+    OfertasComponent,
+    PuntosVentaComponent,
+    EventosComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
